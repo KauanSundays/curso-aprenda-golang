d4ef888d8d8b88e504ee52d2a390bce98b985c33
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-02-16 20:38:41 -0300

    wip

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
-var nome string
+var (
+	nome string
+	n1 int
+	n2 int32
+)
 
 func main() {
-	nome = "Lamar"
-	fmt.Println("Hello", nome)
+	var a, b, c = true, "toma", 123
+
+	fmt.Println(a, b, c)
 }
