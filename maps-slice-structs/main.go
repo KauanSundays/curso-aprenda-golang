f775068480be085f95a666eb83ba489ee828e095
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-02-17 23:36:35 -0300

    wip

--- maps-slice-structs/main.go
+++ maps-slice-structs/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	nomes := []string{"Ana", "Bruno", "Carlos"}
+	fmt.Println(nomes)
 
+	nomes := append(nomes,  "Diana") //Adiciona um elemento no final da slice
+	fmt.Println(nomes)
 }
