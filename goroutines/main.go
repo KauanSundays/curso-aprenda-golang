a13914d01e6d8231b9d8cb2f6ec100fbf4000d81
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-02-26 20:06:44 -0300

    wip

---
+++ goroutines/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func numeros() {
+	for i := 0; i < 10; i++ { // inicia a variavel, vezes, continuidade
+		fmt.Printf( "%d ", i)
+		time.Sleep(time.Millisecond + 150)
+	}
+}
+
+func letras() {
+	for l := 'a'; l < 'j'; l++ { // inicia a variavel, vezes, continuidade
+		fmt.Printf( "%c ", l)
+		time.Sleep(time.Millisecond + 170)
+	}
+}
+
+func main() {
+	numeros()
+	letras()
+}
